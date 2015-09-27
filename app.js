f01c71395a0a31923d1e85055eabcfd05930e20c
Harris Tzovanakis <me@drjova.com>
2015-09-27 12:44:02 +0200

    app: socket.io and event emitter

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 // Configure the server
@@ -9,26 +10,42 @@ server.listen(4000);
 // Watch directory
 var watch = require('watch');
 
+// Event emiter
+var events = require("events");
+var appEvents = new events.EventEmitter();
+
 // String IO setup and listeners
 var io = require('socket.io')(server);
-// On new connectin
+// On new connection
 io.on('connection', function(socket) {
+
   socket.emit('connection', {
     message: 'Connected'
+  });
+
+  // Emit the new image
+  appEvents.on('handshake.new.picture', function(data) {
+    socket.emit('handshake.new.picture', data);
   });
 });
 
 // App settings
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.static(__dirname + '/watch'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.render('index');
 });
 
-watch.createMonitor('./watch', function(monitor) {
+watch.createMonitor('./public/photos', function(monitor) {
   console.log('initializing watch');
+  monitor.on("created", function(f, stat) {
+    appEvents.emit('handshake.new.picture', {
+      file: f
+    });
+  });
 });
 
 // Listen to port 3000
